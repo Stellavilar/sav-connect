65693437b94ceb0653b1817bf27b2c7956887f8d
Stella Vilar <vilar.stella@gmail.com>
2020-11-10 20:34:22 +0100

    Sql correction

--- BACK/data/updateTag.sql
+++ BACK/data/updateTag.sql
@@ -1,6 +1,6 @@
 BEGIN;
 
-ALTER  TABLE "order_repair_tag" ADD COLUMN "order_number_id" text REFERENCES;
+ALTER  TABLE "order_repair_tag" ADD COLUMN "order_number_id" text;
 
 ALTER TABLE "order_repair_tag"
     ADD CONSTRAINT order_repair_tag_order_repair_order_number_fkey FOREIGN KEY (order_number_id)
